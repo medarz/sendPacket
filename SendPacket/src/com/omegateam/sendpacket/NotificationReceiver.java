4830b446a09437866a654e15ded3be6e4b4e57c1
mario_medina <mario.medarz@gmail.com>
2014-03-27 18:48:05 -0600

    Se agregan carpetas que no habían sido incluidas

---
+++ SendPacket/src/com/omegateam/sendpacket/NotificationReceiver.java
@@ -0,0 +1,13 @@
+package com.omegateam.sendpacket;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class NotificationReceiver extends Activity {
+	
+	  @Override
+	  protected void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.notification);
+	  }
+	} 
